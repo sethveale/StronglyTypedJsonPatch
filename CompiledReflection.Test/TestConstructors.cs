4ec6b07ede29cbd440dadb09782d79823407c813
Seth Veale <seth.veale@connectintouch.com>
2017-02-12 10:48:23 +1300

    Add AsLambda and tests to CompiledReflection.Constructors

---
+++ CompiledReflection.Test/TestConstructors.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace CompiledReflection.Test
+{
+    [TestFixture]
+    public class TestConstructors
+    {
+        private const long Ticks = 120398120;
+        private const DateTimeKind Kind = DateTimeKind.Utc;
+
+        private const int Year = 2017, Month = 4, Day = 20;
+        private static readonly Calendar Calendar = new GregorianCalendar();
+
+        #region AsFunc
+
+        [Test]
+        public void TestFuncForConstructorThatDoesntExist()
+        {
+            Assert.That(
+                () => Constructors.AsFunc<bool, DateTime>()(true),
+                Throws.Exception
+            );
+        }
+
+        [Test]
+        public void TestFunc1()
+        {
+            Assert.That(
+                Constructors.AsFunc<long, DateTime>()(Ticks),
+                Is.EqualTo(new DateTime(Ticks))
+            );
+        }
+
+        [Test]
+        public void TestFunc2()
+        {
+            Assert.That(
+                Constructors.AsFunc<long, DateTimeKind, DateTime>()(Ticks, Kind),
+                Is.EqualTo(new DateTime(Ticks, Kind))
+            );
+        }
+
+        [Test]
+        public void TestFunc3()
+        {
+            Assert.That(
+                Constructors.AsFunc<int, int, int, DateTime>()(Year, Month, Day),
+                Is.EqualTo(new DateTime(Year, Month, Day))
+            );
+        }
+
+        [Test]
+        public void TestFunc4()
+        {
+            Assert.That(
+                Constructors.AsFunc<int, int, int, Calendar, DateTime>()(Year, Month, Day, Calendar),
+                Is.EqualTo(new DateTime(Year, Month, Day, Calendar))
+            );
+        }
+
+        #endregion
+
+        #region AsLambda
+
+        [Test]
+        public void TestLambdaForConstructorThatDoesntExist()
+        {
+            Assert.That(
+                () => Constructors.AsLambda<bool, DateTime>().Compile()(true),
+                Throws.Exception
+            );
+        }
+
+        [Test]
+        public void TestLambda1()
+        {
+            Assert.That(
+                Constructors.AsLambda<long, DateTime>().Compile()(Ticks),
+                Is.EqualTo(new DateTime(Ticks))
+            );
+        }
+
+        [Test]
+        public void TestLambda2()
+        {
+            Assert.That(
+                Constructors.AsLambda<long, DateTimeKind, DateTime>().Compile()(Ticks, Kind),
+                Is.EqualTo(new DateTime(Ticks, Kind))
+            );
+        }
+
+        [Test]
+        public void TestLambda3()
+        {
+            Assert.That(
+                Constructors.AsLambda<int, int, int, DateTime>().Compile()(Year, Month, Day),
+                Is.EqualTo(new DateTime(Year, Month, Day))
+            );
+        }
+
+        [Test]
+        public void TestLambda4()
+        {
+            Assert.That(
+                Constructors.AsLambda<int, int, int, Calendar, DateTime>().Compile()(Year, Month, Day, Calendar),
+                Is.EqualTo(new DateTime(Year, Month, Day, Calendar))
+            );
+        }
+
+        #endregion
+    }
+}
--- CompiledReflection/Constructors.cs
+++ CompiledReflection/Constructors.cs
@@ -9,72 +9,137 @@ namespace CompiledReflection
     /// <summary>
     ///     Finds and wraps constructors that match the type parameters of <see cref="Func{TResult}" />s
     /// </summary>
-    public static class ConstructorFunc
+    public static class Constructors
     {
+        #region AsFunc
+
         /// <summary>
         ///     Creates a func that wraps a constructor for <typeparamref name="TResult" /> that all the given parameter types
         ///     (in order).
         /// </summary>
-        public static Func<T1, TResult> From<T1, TResult>()
+        public static Func<T1, TResult> AsFunc<T1, TResult>()
         {
             Contract.Ensures(Contract.Result<Func<T1, TResult>>() != null);
 
             return FindAndWrapConstructor(
                 (body, args) => Expression.Lambda<Func<T1, TResult>>(body, args).Compile()
-                );
+            );
         }
 
         /// <summary>
         ///     Creates a func that wraps a constructor for <typeparamref name="TResult" /> that all the given parameter types
         ///     (in order).
         /// </summary>
-        public static Func<T1, T2, TResult> From<T1, T2, TResult>()
+        public static Func<T1, T2, TResult> AsFunc<T1, T2, TResult>()
         {
             Contract.Ensures(Contract.Result<Func<T1, T2, TResult>>() != null);
 
             return FindAndWrapConstructor(
                 (body, args) => Expression.Lambda<Func<T1, T2, TResult>>(body, args).Compile()
-                );
+            );
         }
 
         /// <summary>
         ///     Creates a func that wraps a constructor for <typeparamref name="TResult" /> that all the given parameter types
         ///     (in order).
         /// </summary>
-        public static Func<T1, T2, T3, TResult> From<T1, T2, T3, TResult>()
+        public static Func<T1, T2, T3, TResult> AsFunc<T1, T2, T3, TResult>()
         {
             Contract.Ensures(Contract.Result<Func<T1, T2, T3, TResult>>() != null);
 
             return FindAndWrapConstructor(
                 (body, args) => Expression.Lambda<Func<T1, T2, T3, TResult>>(body, args).Compile()
-                );
+            );
         }
 
         /// <summary>
         ///     Creates a func that wraps a constructor for <typeparamref name="TResult" /> that all the given parameter types
         ///     (in order).
         /// </summary>
-        public static Func<T1, T2, T3, T4, TResult> From<T1, T2, T3, T4, TResult>()
+        public static Func<T1, T2, T3, T4, TResult> AsFunc<T1, T2, T3, T4, TResult>()
         {
             Contract.Ensures(Contract.Result<Func<T1, T2, T3, T4, TResult>>() != null);
 
             return FindAndWrapConstructor(
                 (body, args) => Expression.Lambda<Func<T1, T2, T3, T4, TResult>>(body, args).Compile()
-                );
+            );
         }
+
+        #endregion
+
+        #region AsLambda
+
+        /// <summary>
+        ///     Creates a func that wraps a constructor for <typeparamref name="TResult" /> that all the given parameter types
+        ///     (in order).
+        /// </summary>
+        public static Expression<Func<T1, TResult>> AsLambda<T1, TResult>()
+        {
+            Contract.Ensures(Contract.Result<Expression<Func<T1, TResult>>>() != null);
+
+            return FindAndWrapConstructor(Expression.Lambda<Func<T1, TResult>>);
+        }
+
+        /// <summary>
+        ///     Creates a func that wraps a constructor for <typeparamref name="TResult" /> that all the given parameter types
+        ///     (in order).
+        /// </summary>
+        public static Expression<Func<T1, T2, TResult>> AsLambda<T1, T2, TResult>()
+        {
+            Contract.Ensures(Contract.Result<Expression<Func<T1, T2, TResult>>>() != null);
+
+            return FindAndWrapConstructor(Expression.Lambda<Func<T1, T2, TResult>>);
+        }
+
+        /// <summary>
+        ///     Creates a func that wraps a constructor for <typeparamref name="TResult" /> that all the given parameter types
+        ///     (in order).
+        /// </summary>
+        public static Expression<Func<T1, T2, T3, TResult>> AsLambda<T1, T2, T3, TResult>()
+        {
+            Contract.Ensures(Contract.Result<Expression<Func<T1, T2, T3, TResult>>>() != null);
+
+            return FindAndWrapConstructor(Expression.Lambda<Func<T1, T2, T3, TResult>>);
+        }
+
+        /// <summary>
+        ///     Creates a func that wraps a constructor for <typeparamref name="TResult" /> that all the given parameter types
+        ///     (in order).
+        /// </summary>
+        public static Expression<Func<T1, T2, T3, T4, TResult>> AsLambda<T1, T2, T3, T4, TResult>()
+        {
+            Contract.Ensures(Contract.Result<Expression<Func<T1, T2, T3, T4, TResult>>>() != null);
+
+            return FindAndWrapConstructor(Expression.Lambda<Func<T1, T2, T3, T4, TResult>>);
+        }
+
+        #endregion
 
         private static readonly ConcurrentDictionary<Type, object> Cache = new ConcurrentDictionary<Type, object>();
 
-        private static TFunc FindAndWrapConstructor<TFunc>(Func<Expression, ParameterExpression[], TFunc> compile)
-            where TFunc : class
+        private static TFuncOrLambda FindAndWrapConstructor<TFuncOrLambda>(
+            Func<Expression, ParameterExpression[], 
+            TFuncOrLambda> compile)
+            where TFuncOrLambda : class
         {
-            var funcType = typeof(TFunc);
-            var func = (TFunc) Cache.GetOrAdd(funcType, ft => FindAndWrapConstructor(ft, compile));
+            var funcOrLambdaType = typeof(TFuncOrLambda);
+
+            Type funcType;
+            if (funcOrLambdaType.GetGenericTypeDefinition() == typeof(Expression<>))
+            {
+                Contract.Assume(funcOrLambdaType.GenericTypeArguments != null && funcOrLambdaType.GenericTypeArguments.Length > 0);
+                funcType = funcOrLambdaType.GenericTypeArguments[0];
+            }
+            else
+                funcType = funcOrLambdaType;
+
+            var func = (TFuncOrLambda)Cache.GetOrAdd(funcOrLambdaType, _ => FindAndWrapConstructor(funcType, compile));
             Contract.Assume(func != null);
             return func;
         }
 
-        private static TFunc FindAndWrapConstructor<TFunc>(Type funcType, Func<Expression, ParameterExpression[], TFunc> compile)
+        private static TFunc FindAndWrapConstructor<TFunc>(Type funcType,
+            Func<Expression, ParameterExpression[], TFunc> compile)
         {
             Contract.Requires(funcType != null);
             Contract.Requires(compile != null);
